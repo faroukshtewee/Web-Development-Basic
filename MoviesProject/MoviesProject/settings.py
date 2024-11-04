8270fd9230f5e7674aa066921a08f5658e3c4c2c
faroukshtewee <farok95@hotmail.com>
2024-11-04 22:17:47 +0200

    docker compose

--- MoviesProject/MoviesProject/settings.py
+++ MoviesProject/MoviesProject/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'MoviesProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, 'db65413.sqlite3')),
+        "USER": os.environ.get("SQL_USER", "myuser"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "myuserpassword"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "3306"),
     }
 }
 
